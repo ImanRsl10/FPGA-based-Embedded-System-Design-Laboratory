c980cbce16b9ac98bf468cba0024b4b3a839a7c6
Navid Razaghi <76059045+navidrazaghi@users.noreply.github.com>
2023-10-29 22:40:26 +0330

    Last try of receiver at laboratory
    We saw some incorrect results on board but it was encouraging.

--- async_receiver.v
+++ async_receiver.v
@@ -3,62 +3,59 @@ module async_receiver(
     input rst,
     input RxD_in,
     output reg RxD_data_ready,
-    output reg [7:0] RxD_data  // data received, valid only (for one clock cycle) when RxD_data_ready is asserted
+    output [7:0] out  // data received, valid only (for one clock cycle) when RxD_data_ready is asserted
 );
 
     parameter ClkFrequency = 50000000;
     parameter Baud = 115200;
     parameter Oversampling = 4;	// needs to be a power of 2
 
-    wire BaudTick, co1, co2, RxD, co;
-    wire [2:0] count;
-    reg sh_en1, sh_en2, clr1, clr2;
-    reg enable;
-    reg [1:0] count1;
-    reg [3:0] count2;
-
-    reg [2:0] ps, ns;
-    parameter [2:0] idle = 3'b000, start = 3'b001, counter = 3'b010, transmit = 3'b011, RxD_ready = 3'b100;
+    wire BaudTick, co1, RxD, co;
+    reg [3:0] count1;
+    reg [1:0] ps, ns;
+	reg [7:0] RxD_data;
+	assign out = RxD_data;
+    parameter [1:0] idle = 2'b00, transmit = 2'b01, RxD_ready = 2'b10;
 
     BaudTickGen #(ClkFrequency, Baud, Oversampling) tickgen4(clk, rst, 1'b0, BaudTick);
-    overSamplingHandler handler(.clk(clk), .rst(rst), .RxD_raw(RxD_in), .BaudTick(BaudTick), .RxD(RxD), .co(co), .count(count));
+    overSamplingHandler handler(.clk(clk), .rst(rst), .RxD_raw(RxD_in), .BaudTick(BaudTick), .RxD(RxD), .co(co));
 
-    always @(ps, co2, RxD, BaudTick) begin
+    always @(ps, co1, co, RxD) begin
         case(ps)
-            idle: ns = RxD ? idle : transmit;
-            //start: ns = RxD ? counter : transmit; //start;
-			// counter: ns = transmit;
-            transmit: begin ns = co2 ? RxD_ready : transmit; RxD_data <= co ? {RxD, RxD_data[7:1]} : RxD_data; end 
-            RxD_ready: begin ns = idle; RxD_data_ready = 1'b1; end
+            idle: ns <= (~RxD & co) ? transmit : idle;
+            transmit: ns <= co1 ? RxD_ready : transmit;
+            RxD_ready: ns <= idle;
         endcase
     end
 
-    always @(ps) begin
-        {enable, RxD_data_ready, sh_en1, sh_en2, clr1, clr2} = 6'b0;
+    always @ (ps) begin
+        RxD_data_ready = 1'b0;
         case(ps)
-            idle: {clr1, clr2} = 2'b11;
-			//counter: enable = 1'b1;
-            transmit: begin ns = co2 ? RxD_ready : transmit; RxD_data <= co ? {RxD, RxD_data[7:1]} : RxD_data; end //Co
-            RxD_ready: begin ns = idle; RxD_data_ready = 1'b1; end
+            RxD_ready: RxD_data_ready = 1'b1;
         endcase
     end
 
     always @(posedge clk) begin
-        if(rst) ps <= idle;
-        else ps <= ns;
+        if (rst)
+            ps <= idle;
+        else
+            ps <= ns;
     end
 
     always @(posedge clk) begin
-        if(rst) count1 <= 2'b00;
-        else if(BaudTick & (count == 3'b011)) count1 <= count1 + 1'b1;
+        if(rst | co1)
+            count1 <= 4'b0;
+        else if(co)
+            count1 <= count1 + 1'b1;
     end
 
-    always @(posedge clk) begin
-        if(rst | co2) count2 <= 4'b0;
-        else if(BaudTick & (count == 3'b011)) count2 <= count2 + 1'b1;
+    always @(posedge clk)begin
+        if(co & (ps == transmit))
+            RxD_data <= {RxD, RxD_data[7:1]};
+        else
+            RxD_data <= RxD_data;
     end
 
-    assign co1 = &count1;
-    assign co2 = (count2 == 4'b1001);
+    assign co1 = (count1 == 4'b1001);
 
 endmodule
