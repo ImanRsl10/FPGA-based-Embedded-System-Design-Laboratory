bc6917f582b988c1aa96a3943035541d9da6e124
zahrahojati19 <129644190+zahrahojati19@users.noreply.github.com>
2023-11-02 12:39:55 +0330

    Update Shift_Reg.v

--- Shift_Reg.v
+++ Shift_Reg.v
@@ -1,10 +1,10 @@
 module Shift_Reg(clk, rst, ld, inp , outp);
 	input clk, rst, ld;
-	input[7:0] inp;
+	input [7:0]inp;
 	output reg[15:0] outp;
 
 	always@(posedge clk)begin
-		if(rst) outp <= 16'b0;
-		else if(ld) outp <= {outp[7:0], inp};
+		if(rst) outp = 16'b0;
+		else if(ld) outp = {outp[7:0], inp};
 	end 
 endmodule
