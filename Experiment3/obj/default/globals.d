d6edcc88c23e14aaeefe13776fcd979db00eaad3
ImanRsl10 <iman.rasouli10@gmail.com>
2024-11-05 12:34:41 +0330

    Renamed folder from old-folder to new-folder

---
+++ Experiment3/obj/default/globals.d
@@ -0,0 +1,85 @@
+obj/default/globals.o: globals.c globals.h \
+  ../LAB3_bsp//drivers/inc/altera_up_avalon_parallel_port.h \
+  ../LAB3_bsp//HAL/inc/sys/alt_dev.h ../LAB3_bsp/system.h \
+  ../LAB3_bsp/linker.h ../LAB3_bsp//HAL/inc/sys/alt_llist.h \
+  ../LAB3_bsp//HAL/inc/alt_types.h \
+  ../LAB3_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../LAB3_bsp//HAL/inc/sys/alt_llist.h ../LAB3_bsp//HAL/inc/alt_types.h \
+  ../LAB3_bsp//HAL/inc/sys/alt_alarm.h \
+  ../LAB3_bsp//HAL/inc/sys/alt_llist.h \
+  ../LAB3_bsp//HAL/inc/priv/alt_alarm.h \
+  ../LAB3_bsp//HAL/inc/sys/alt_warning.h \
+  ../LAB3_bsp//drivers/inc/altera_up_avalon_parallel_port_regs.h \
+  ../LAB3_bsp//HAL/inc/io.h ../LAB3_bsp//HAL/inc/alt_types.h \
+  ../LAB3_bsp//drivers/inc/altera_up_avalon_character_lcd.h \
+  ../LAB3_bsp//drivers/inc/altera_up_avalon_audio.h \
+  ../LAB3_bsp//HAL/inc/alt_types.h ../LAB3_bsp//HAL/inc/sys/alt_dev.h \
+  ../LAB3_bsp//drivers/inc/altera_up_avalon_ps2.h \
+  ../LAB3_bsp//drivers/inc/altera_up_avalon_video_character_buffer_with_dma.h \
+  ../LAB3_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h \
+  ../LAB3_bsp//HAL/inc/sys/alt_stdio.h ../LAB3_bsp//HAL/inc/sys/alt_irq.h \
+  ../LAB3_bsp//HAL/inc/nios2.h ../LAB3_bsp//HAL/inc/priv/alt_legacy_irq.h \
+  ../LAB3_bsp/system.h ../LAB3_bsp//HAL/inc/nios2.h \
+  ../LAB3_bsp//HAL/inc/sys/alt_irq.h
+
+globals.h:
+
+../LAB3_bsp//drivers/inc/altera_up_avalon_parallel_port.h:
+
+../LAB3_bsp//HAL/inc/sys/alt_dev.h:
+
+../LAB3_bsp/system.h:
+
+../LAB3_bsp/linker.h:
+
+../LAB3_bsp//HAL/inc/sys/alt_llist.h:
+
+../LAB3_bsp//HAL/inc/alt_types.h:
+
+../LAB3_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../LAB3_bsp//HAL/inc/sys/alt_llist.h:
+
+../LAB3_bsp//HAL/inc/alt_types.h:
+
+../LAB3_bsp//HAL/inc/sys/alt_alarm.h:
+
+../LAB3_bsp//HAL/inc/sys/alt_llist.h:
+
+../LAB3_bsp//HAL/inc/priv/alt_alarm.h:
+
+../LAB3_bsp//HAL/inc/sys/alt_warning.h:
+
+../LAB3_bsp//drivers/inc/altera_up_avalon_parallel_port_regs.h:
+
+../LAB3_bsp//HAL/inc/io.h:
+
+../LAB3_bsp//HAL/inc/alt_types.h:
+
+../LAB3_bsp//drivers/inc/altera_up_avalon_character_lcd.h:
+
+../LAB3_bsp//drivers/inc/altera_up_avalon_audio.h:
+
+../LAB3_bsp//HAL/inc/alt_types.h:
+
+../LAB3_bsp//HAL/inc/sys/alt_dev.h:
+
+../LAB3_bsp//drivers/inc/altera_up_avalon_ps2.h:
+
+../LAB3_bsp//drivers/inc/altera_up_avalon_video_character_buffer_with_dma.h:
+
+../LAB3_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h:
+
+../LAB3_bsp//HAL/inc/sys/alt_stdio.h:
+
+../LAB3_bsp//HAL/inc/sys/alt_irq.h:
+
+../LAB3_bsp//HAL/inc/nios2.h:
+
+../LAB3_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../LAB3_bsp/system.h:
+
+../LAB3_bsp//HAL/inc/nios2.h:
+
+../LAB3_bsp//HAL/inc/sys/alt_irq.h:
